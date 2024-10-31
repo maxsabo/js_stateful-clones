60300b8b37cc2aaf063404971dacd24d79800c8f
Maksym Sabov <maksim50.97@gmail.com>
2024-10-31 12:42:33 +0200

    task implementation

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,43 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = Object.assign({}, state);
+
+  const eachStateResult = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      addProperties(stateCopy, action.extraData);
+
+      eachStateResult.push({ ...stateCopy });
+    } else if (action.type === 'removeProperties') {
+      removeProperties(stateCopy, action.keysToRemove);
+
+      eachStateResult.push({ ...stateCopy });
+    } else if (action.type === 'clear') {
+      clear(stateCopy);
+
+      eachStateResult.push({ ...stateCopy });
+    }
+  }
+
+  return eachStateResult;
+}
+
+function addProperties(stateCopy, extraData) {
+  Object.assign(stateCopy, extraData);
+}
+
+function removeProperties(stateCopy, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete stateCopy[key];
+  }
+}
+
+function clear(stateCopy) {
+  for (const property in stateCopy) {
+    delete stateCopy[property];
+  }
 }
 
 module.exports = transformStateWithClones;
